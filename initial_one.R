660e6d39487e2cd0723b9df94379820481e5a33a
alwinruby <alwinsolanky@gmail.com>
2022-02-05 10:57:14 +0000

    percentiles

--- initial_one.R
+++ initial_one.R
@@ -1,6 +1,11 @@
-qplot(extrovert, revenue, data = emp_sim)
-qplot(extrovert, revenue, data = emp_sim, geom = 'boxplot')
+clearqplot(extrovert, revenue, data = emp_sim, geom = 'boxplot')
 qplot(extrovert, revenue, data = emp_sim, geom = 'violin')
 emp_sim %>% summarise(avg = mean(revenue))
 emp_sim %>% summarise(avg = mean(revenue), med = median(revenue), count = n())
 emp_sim %>% group_by(education) %>% summarise(avg = mean(revenue), med = median(revenue), count = n())
+no_prior = emp_sim %>% filter(prior_sales == 0)
+no_prior
+q10 = quantile(emp_sim$revenue, 0.1)
+q90 = quantile(emp_sim$revenue, 0.9)
+q10
+q90
